8e81d97cf6e8e693df2ebc5c967f2912a1904c6c
Suyen-Shrestha <suyencrestha@gmail.com>
2019-11-05 11:18:22 +0545

    checkout component added but not completed yet.

--- src/ecommerce/carts/urls.py
+++ src/ecommerce/carts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import cart_home, cart_update
+from .views import cart_home, cart_update, checkout_home
 
 app_name = 'cart'
 
 urlpatterns = [
     path('', cart_home, name='home'),
+    path('checkout/', checkout_home, name='checkout'),
     path('update/', cart_update, name='update'),
     # path('products/<int:pk>', ProductDetailView.as_view()),
     # path('featured/', ProductFeaturedListView.as_view()),
--- src/ecommerce/carts/views.py
+++ src/ecommerce/carts/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Cart
+from orders.models import Order
 from products.models import Product
 
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    products = cart_obj.products.all()
-    total = 0
-    for x in products:
-        total += x.price
-    print(total)
-    cart_obj.total = total
-    cart_obj.save()
-    context = {
-        'cart': cart_obj
-    }
-    return render(request, "carts/home.html", context)
+    return render(request, "carts/home.html", {'cart': cart_obj})
 
 
 def cart_update(request):
@@ -23,14 +14,24 @@ def cart_update(request):
     if product_id is not None:
         try:
             product_obj = Product.objects.get(id=product_id)
-        except:
+        except Product.DoesNotExist:
             print("Show message to user, product is gone?")
             return redirect("cart:home")
         cart_obj, new_obj = Cart.objects.new_or_get(request)
-
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
         else:
             cart_obj.products.add(product_obj)
-
+        request.session['cart_items'] = cart_obj.products.count()
     return redirect("cart:home")
+
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+
+    return render(request, "carts/checkout.html", {"object": order_obj})
